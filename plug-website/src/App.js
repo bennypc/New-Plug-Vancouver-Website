1dbf69e4285efa75361155b6a43bc1e22d62ccc0
bennypc <52959626+bennypc@users.noreply.github.com>
2022-11-07 20:44:01 -0800

    added burger menu

--- plug-website/src/App.js
+++ plug-website/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Slider from "react-slick";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import BurgerMenu from "./components/burger";
 import "slick-carousel/slick/slick.css";
 
 import "slick-carousel/slick/slick-theme.css";
@@ -55,6 +56,7 @@ function Header() {
               
               <img className='w-10 h-auto my-4'
               src={require('./images/hamburger-menu.png')} alt="Hamburger Menu" onClick={() => openNav()}/>
+              
             </div>
             
           </div>
@@ -116,18 +118,17 @@ function Events() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 700,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          initialSlide: 3
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          initialSlide: 1
         }
       },
       {
@@ -230,11 +231,10 @@ function Events() {
 function Media() {
   return (
     <div id='media' className='text-white text-center h-[300px] text-5xl'>
-      <h1>MEDIA</h1>
+      <h1 className='my-8 text-white text-center text-5xl italic font-bold'>WATCH</h1>
     </div>
   )
 }
-
 
 function Contact() {
   return (
@@ -418,7 +418,6 @@ function App() {
   return (
     <>
       
-
       <Logo/>
 
       {/* <div>
@@ -501,6 +500,8 @@ function App() {
         </div>
       </div>
       
+      <BurgerMenu />
+
       <Home/>
       
       <Events/>
---
+++ plug-website/src/Article.js
@@ -0,0 +1,9 @@
+const Article = () => {
+    return (
+      <div>
+        <h1>Hello from Article</h1>
+      </div>
+    );
+  };
+  
+  export default Article;
