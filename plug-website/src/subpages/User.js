0da4b093188f01790294d8ef0faf8a6223b11e74
bennypc <benny.pincha@gmail.com>
2022-11-19 20:23:38 -0800

    fixed font sizes

--- plug-website/src/subpages/User.js
+++ plug-website/src/subpages/User.js
@@ -20,15 +20,15 @@ const User = () => {
     <div className="bg-black ">
       <div className="flex min-h-full items-center justify-center py-24 px-8 sm:px-6 lg:px-8"></div>
       <img
-        className="mx-auto h-[150px] w-auto"
+        className="mx-auto h-[125px] w-auto"
         src={require("../images/logos/PlugWhite.png")}
         alt="Your Company"
       />
 
-      <h1 className=" text-white text-3xl text-center mt-8 mx-4">
+      <h1 className=" text-white text-4xl text-center mt-8 mx-4">
         Welcome, {firstName}
       </h1>
-      <h2 className=" text-white text-2xl text-center mt-6 mx-6 font-normal">
+      <h2 className=" text-white text-3xl text-center mt-6 mx-6 font-normal">
         Stay tuned for more features...
       </h2>
     </div>
