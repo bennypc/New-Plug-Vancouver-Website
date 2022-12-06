6a12a9946e41b8cfc136a99d43cc31ec5cf2e4c6
bennypc <benny.pincha@gmail.com>
2022-12-05 18:20:11 -0800

    fixed empty form bug for contact

--- plug-website/src/subpages/About.js
+++ plug-website/src/subpages/About.js
@@ -20,29 +20,19 @@ const AboutPage = () => {
     from_name: fromName,
     message: message,
   };
-
   const sendEmail = (e) => {
     e.preventDefault();
 
-    if (fromName || message === "") {
-      alert("Please enter your name and a message!");
-    } else {
-      emailjs
-        .send(
-          "service_38dsvu9",
-          templateID,
-          templateParams,
-          "JYTHfEmzwQQJLqNA2"
-        )
-        .then(
-          function (response) {
-            console.log("SUCCESS!", response.status, response.text);
-          },
-          function (error) {
-            console.log("FAILED...", error);
-          }
-        );
-    }
+    emailjs
+      .send("service_38dsvu9", templateID, templateParams, "JYTHfEmzwQQJLqNA2")
+      .then(
+        function (response) {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
   };
 
   return (
@@ -95,39 +85,43 @@ const AboutPage = () => {
           CONTACT US
         </h1>
 
-        <form ref={form} onSubmit={sendEmail}>
-          <label>
-            <h2 className="my-4">Name</h2>
-          </label>
-          <input
-            style={{ fontFamily: "Helvetica" }}
-            type="text"
-            name="user_name"
-            value={fromName}
-            onChange={(e) => setFromName(e.target.value)}
-            placeholder="Your Name"
-            className="contact-input w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-          />
+        <div className="flex align-middle justify-center">
+          <form ref={form} onSubmit={sendEmail}>
+            <label>
+              <h2 className="my-4">Name</h2>
+            </label>
+            <input
+              style={{ fontFamily: "Helvetica" }}
+              type="text"
+              name="user_name"
+              value={fromName}
+              onChange={(e) => setFromName(e.target.value)}
+              placeholder="Your Name"
+              required
+              className="contact-input py-5 md:mx-0 md:w-[600px] lg:w-[900px] bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 md:py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
 
-          <label>
-            <h2 className="my-4">Message</h2>
-          </label>
-          <textarea
-            style={{ fontFamily: "Helvetica" }}
-            name="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder="Your Message"
-            className="contact-input w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-          />
-          <button
-            type="submit"
-            value="Send"
-            className="mt-8 mb-16 flex mx-auto text-white bg-[#e6007e] border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-          >
-            Send Message!
-          </button>
-        </form>
+            <label>
+              <h2 className="my-4">Message</h2>
+            </label>
+            <textarea
+              style={{ fontFamily: "Helvetica" }}
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Your Message"
+              required
+              className="contact-input py-3 w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 md:py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            />
+            <button
+              type="submit"
+              value="Send"
+              className="mt-8 mb-16 flex mx-auto text-white bg-[#e6007e] border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+              Send Message!
+            </button>
+          </form>
+        </div>
 
         <h1 className="mt-4 md:mt-8 mb-2 text-black text-center text-[38px] md:text-5xl italic font-bold">
           OUR PEOPLE
