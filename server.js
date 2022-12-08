e8724082dd050ef4dc7d771cbb7bdc8b4702f286
bennypc <benny.pincha@gmail.com>
2022-12-08 15:21:47 -0800

    heroku

--- server.js
+++ server.js
@@ -8,42 +8,48 @@ const app = express();
 
 app.use(cors());
 
-// app.get("/", (req, res) => {
-//   res.send("hello world");
-// });
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-// app.use(express.static(path.join(__dirname, "..", "plug-website", "build")));
-// //app.use(express.static(path.join(__dirname, "..", "plug-website")));
+app.use(express.static(path.join(__dirname, "..", "plug-website", "build")));
+//app.use(express.static(path.join(__dirname, "..", "plug-website")));
 
-// //const YOUR_DOMAIN = "http://localhost:4242";
-// const YOUR_DOMAIN = "http://plugvancouver.com";
+//const YOUR_DOMAIN = "http://localhost:4242";
+const YOUR_DOMAIN = "http://plugvancouver.com";
 
-// app.post("/create-checkout-session", async (req, res) => {
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: [
-//       {
-//         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-//         price: "price_1MAlN7JeUNpSjLZt3Jd4dfJ9",
-//         quantity: 1,
-//       },
-//     ],
-//     mode: "payment",
-//     success_url: `${YOUR_DOMAIN}?success=true`,
-//     cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-//     automatic_tax: { enabled: true },
-//   });
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: "price_1MAlN7JeUNpSjLZt3Jd4dfJ9",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    automatic_tax: { enabled: true },
+  });
 
-//   res.redirect(303, session.url);
-// });
+  res.redirect(303, session.url);
+});
 
-// // create a GET route
-// app.get("*", function (req, res) {
-//   res.sendFile("index.html", {
-//     root: path.join(__dirname, "../plug-website/build/"),
-//   });
-// });
+// create a GET route
+app.get("*", function (req, res) {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "../plug-website/build/"),
+  });
+});
 
-app.listen(process.env.PORT || 4242, () => console.log("Server has started"));
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
 
 // const storeItems = new Map([
 //   [1, { priceInCents: 1000, name: "Event ticket 1" }],
