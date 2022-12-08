cb28cbc5f819694cb37c41b59e95264f9e40b719
bennypc <benny.pincha@gmail.com>
2022-12-08 02:09:38 -0800

    fixed css link

--- plug-website/src/subpages/shop/OrderSuccess.js
+++ plug-website/src/subpages/shop/OrderSuccess.js
@@ -1,14 +1,16 @@
 import React from "react";
-import "../subcss/about.css";
+import "./subcss/about.css";
 import ShopItem from "../../components/ShopItem.js";
 
 const ShopPage = () => {
   return (
     <div className="flex justify-center">
       <div className="about-container">
-        <h1>SHOP</h1>
-
-        <ShopItem />
+        <h1>Thanks for your order!</h1>
+        <p>
+          We appreciate your business! If you have any questions, please email
+          <a href="mailto:orders@example.com">orders@example.com</a>.
+        </p>
       </div>
     </div>
   );
