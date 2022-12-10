4780795660fac5799c11854fbe7c9e185f0b66b2
bennypc <benny.pincha@gmail.com>
2022-12-10 01:57:04 -0800

    shop heroku link

--- plug-website/src/components/ShopItem.js
+++ plug-website/src/components/ShopItem.js
@@ -199,7 +199,10 @@ export default function Example() {
                 </a>
               </div> */}
               <div className="mt-10">
-                <form action="/create-checkout-session" method="POST">
+                <form
+                  action="https://plug-vancouver.herokuapp.com/create-checkout-session"
+                  method="POST"
+                >
                   <button
                     type="submit"
                     className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
